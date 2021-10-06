2a9ecef14ea3dcc292c3557b961e72d521c57115
Leandro Vieira <leandrown@outlook.com>
2021-10-06 16:09:55 -0300

    Add project files.

---
+++ TesteLogica/Network.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteLogica
+{
+   class Network
+   {
+      public Network(int element)
+      {
+            
+      }
+      public int connect()
+      {
+         return 0;
+      }
+      public bool query(int element)
+      {
+         if (connect() == 0) return true;
+         return false;
+      }
+   }
+}
